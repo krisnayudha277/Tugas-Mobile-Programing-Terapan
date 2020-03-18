b29a43e56f81cea5a848837603713cf88e527167
krisnayudha277 <60349598+krisnayudha277@users.noreply.github.com>
2020-03-18 16:37:04 +0700

    Update Menu.kt

--- app/src/main/java/com/example/utsmobileprograming/Menu.kt
+++ app/src/main/java/com/example/utsmobileprograming/Menu.kt
@@ -21,5 +21,40 @@ class Menu : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
 
         val users = ArrayList<User>()
+        
+        
+         AndroidNetworking.get("http://192.168.43.210/cobarepo/penduduk-json.php")
+            .setPriority(Priority.MEDIUM)
+            .build()
+            .getAsJSONObject(object : JSONObjectRequestListener {
+                override fun onResponse(response: JSONObject) {
+                    Log.e("_kotlinResponse", response.toString())
+
+                    val jsonArray = response.getJSONArray("result")
+                    for (i in 0 until jsonArray.length()) {
+                        val jsonObject = jsonArray.getJSONObject(i)
+                        Log.e("_kotlinTitle", jsonObject.optString("id_data"))
+
+                        // txt1.setText(jsonObject.optString("shubuh"))
+                        var isi1 = jsonObject.optString("nama_penduduk").toString()
+                        var isi2 = jsonObject.optString("ttl_penduduk").toString()
+                        var isi3 = jsonObject.optString("hp_penduduk").toString()
+                        var isi4 = jsonObject.optString("alamat_penduduk").toString()
+
+                        users.add(User("$isi1", "$isi2", "$isi3", "$isi4"))
+
+
+                    }
+
+                    val adapter = CustomAdapter(users)
+                    recyclerView.adapter = adapter
+
+
+                }
+
+                override fun onError(anError: ANError) {
+                    Log.i("_err", anError.toString())
+                }
+            })
     }
 }
